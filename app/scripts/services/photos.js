6a150a23efcb6fb2a70b0f3ba8c3101f88cd093c
nerdfiles <nerdfiles@gmail.com>
2016-04-21 12:20:53 -0500

    Cleanup.

--- app/scripts/services/photos.js
+++ app/scripts/services/photos.js
@@ -1,20 +1,22 @@
 define(['interface'], function (__interface__) {
+  function photosService ($http) {
+    var serviceInterface = {};
+    serviceInterface.getPhotos = function () {
+    };
+    serviceInterface.addPhotos = function () {
+    };
+    serviceInterface.updatePhotos = function () {
+    };
+    serviceInterface.removePhotos = function () {
+    };
+
+
+    return serviceInterface;
+  }
+
   __interface__.service('photos', [
     '$http',
-    function photosService ($http) {
-      var serviceInterface = {};
-      serviceInterface.getPhotos = function () {
-      };
-      serviceInterface.addPhotos = function () {
-      };
-      serviceInterface.updatePhotos = function () {
-      };
-      serviceInterface.removePhotos = function () {
-      };
-
-
-      return serviceInterface;
-    }
+    photosService
   ]);
 });
 
--- app/scripts/services/topics.js
+++ app/scripts/services/topics.js
@@ -1,20 +1,22 @@
 define(['interface'], function (__interface__) {
 
+  function topicsService ($http) {
+    var serviceInterface = {};
+    serviceInterface.getTopics = function () {
+    };
+    serviceInterface.addTopics = function () {
+    };
+    serviceInterface.updateTopics = function () {
+    };
+    serviceInterface.removeTopics = function () {
+    };
+
+    return serviceInterface;
+  }
+
   __interface__.service('topics', [
     '$http',
-    function topicsService ($http) {
-      var serviceInterface = {};
-      serviceInterface.getTopics = function () {
-      };
-      serviceInterface.addTopics = function () {
-      };
-      serviceInterface.updateTopics = function () {
-      };
-      serviceInterface.removeTopics = function () {
-      };
-
-      return serviceInterface;
-    }
+    topicsService
   ]);
 });
 
